bf4422be7b8a556cb6754b90c0010104997feb56
carry0987 <carry0987@users.noreply.github.com>
2023-10-07 09:28:43 +0800

    Make composer package

---
+++ src/SimpleCaptcha.php
@@ -0,0 +1,115 @@
+<?php
+namespace carry0987\Captcha;
+
+/**
+ * Simple Captcha Generator
+ */
+class SimpleCaptcha
+{
+    private $option = array();
+
+    public function __construct()
+    {
+    }
+
+    public function setCaptchaOption($option)
+    {
+        $this->option = $option;
+        if (file_exists($this->option['captcha_font']) !== true) {
+            $error = 'Could not find the ttf file !';
+            return $error;
+        }
+    }
+
+    public function checkCaptcha($captcha_code, $submit_code)
+    {
+        if ($this->option['check_sensitive'] === true || $this->option['check_sensitive'] === 1) {
+            $check_result = strcmp($captcha_code, $submit_code);
+        } else {
+            $check_result = strcasecmp($captcha_code, $submit_code);
+        }
+        return $check_result;
+    }
+
+    private function hexToRGB($hex_string)
+    {
+        if (preg_match('/#([a-f0-9]{3}){1,2}\b/i', $hex_string) === true) {
+            $hex_string = trim($hex_string, '#');
+        } else {
+            $hex_string = 142864;
+        }
+        $integar = hexdec($hex_string);
+        $hexresult = array('red' => 0xFF & ($integar >> 0x10), 'green' => 0xFF & ($integar >> 0x8), 'blue' => 0xFF & $integar);
+        return $hexresult;
+    }
+
+    public function generateCaptcha()
+    {
+        //Count character
+        $count = 0;
+        while ($count < $this->option['total_character']) {
+            $this->option['captcha_code'] .= substr($this->option['captcha_letter'], mt_rand(0, strlen($this->option['captcha_letter'])-1), 1);
+            $count++;
+        }
+        $this->option['captcha_font_size'] = $this->option['captcha_image_height'] * 0.65;
+        try {
+            $captcha_image = imagecreate($this->option['captcha_image_width'], $this->option['captcha_image_height']);
+        } catch (\Exception $e) {
+            echo '<h1>'.$e->getMessage().'</h1>';
+            exit();
+        }
+        //Config background,text and noise color
+        $background_color = imagecolorallocate($captcha_image, 255, 255, 255);
+        $array_text_color = $this->hexToRGB($this->option['captcha_text_color']);
+        $this->option['captcha_text_color'] = imagecolorallocate($captcha_image, $array_text_color['red'], $array_text_color['green'], $array_text_color['blue']);
+        $noise_color = $this->hexToRGB($this->option['captcha_noise_color']);
+        $image_noise_color = imagecolorallocate($captcha_image, $noise_color['red'], $noise_color['green'], $noise_color['blue']);
+        //Generate random dots in background of the captcha image
+        for ($count = 0; $count < $this->option['random_captcha_dots']; $count++) {
+            imagefilledellipse(
+                $captcha_image, 
+                mt_rand(0,$this->option['captcha_image_width']), 
+                mt_rand(0,$this->option['captcha_image_height']), 
+                2, 
+                3, 
+                $image_noise_color
+            );
+        }
+        //Generate random lines in background of the captcha image
+        for ($count = 0; $count < $this->option['random_captcha_lines']; $count++) {
+            imageline(
+                $captcha_image, 
+                mt_rand(0, $this->option['captcha_image_width']), 
+                mt_rand(0, $this->option['captcha_image_height']), 
+                mt_rand(0, $this->option['captcha_image_width']), 
+                mt_rand(0, $this->option['captcha_image_height']), 
+                $image_noise_color
+            );
+        }
+        //Create a text box and add 6 captcha letters code in it
+        $text_box = imagettfbbox(
+            $this->option['captcha_font_size'], 
+            0, 
+            $this->option['captcha_font'], 
+            $this->option['captcha_code']
+        );
+        $x = round(($this->option['captcha_image_width']  - $text_box[4]) / 2);
+        $y = round(($this->option['captcha_image_height'] - $text_box[5]) / 2);
+        imagettftext(
+            $captcha_image, 
+            $this->option['captcha_font_size'], 
+            0, $x, $y, 
+            $this->option['captcha_text_color'], 
+            $this->option['captcha_font'], 
+            $this->option['captcha_code']
+        );
+        //Show captcha image in the html page
+        //Defining the image type to be shown in browser window
+        header('Content-Type: image/jpg');
+        //Showing the image
+        imagejpeg($captcha_image);
+        //Destroying the image instance
+        imagedestroy($captcha_image);
+        $_SESSION['captcha'] = $this->option['captcha_code'];
+    }
+}
---
+++ test/index.php
@@ -0,0 +1,83 @@
+<?php
+require dirname(__DIR__).'/vendor/autoload.php';
+
+use carry0987\Captcha\SimpleCaptcha as SimpleCaptcha;
+
+session_start();
+
+$captcha_option = array(
+    'captcha_code' => '',
+    'captcha_image_height' => 60,
+    'captcha_image_width' => 250,
+    'captcha_letter' => 'bcdfghjkmnpqrstvwxyz23456789',
+    'captcha_font' => dirname(__DIR__).'/font/monofont.ttf',
+    'captcha_text_color' => '0x142864',
+    'captcha_noise_color' => '0x142864',
+    'total_character' => 6,
+    'random_captcha_dots' => 50,
+    'random_captcha_lines' => 25,
+    'check_sensitive' => false
+);
+
+$captcha = new SimpleCaptcha;
+$captcha->setCaptchaOption($captcha_option);
+
+if (isset($_GET['rand'])) {
+    echo $captcha->generateCaptcha();
+    exit();
+}
+
+$status = '';
+if (isset($_POST['captcha']) && $_POST['captcha'] !== '') {
+    if ($captcha->checkCaptcha($_SESSION['captcha'], $_POST['captcha']) !== 0) {
+        $status = '<span class="captcha_failed">Entered captcha code does not match! Please try again</span>';
+    } else {
+        $status = '<span class="captcha_success">Your captcha code is match</span>';    
+    }
+} else {
+    $status = '';
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0 ,maximum-scale=1.0, initial-scale=1">
+    <title>PHP Simple Captcha</title>
+    <meta name="author" content="carry0987">
+    <meta name="description" content="This program was made by carry0987">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href="favicon.ico" rel="shortcut icon" />
+</head>
+
+<body>
+    <div id="mainwrapper">
+        <div class="captcha_div">
+            <h1>Create a Simple Captcha</h1>
+            <p class="status"><?=$status;?></p>
+            <form action="" id="form" class="captcha_form" method="post">
+                <label>Enter Captcha :</label>
+                <input type="text" class="captcha_input" name="captcha" id="captcha" placeholder="Captcha" autocomplete="off" />
+                <div class="forgot_password">
+                    <img src="index.php?rand=<?=rand();?>" id='captcha_image'>
+                </div>
+                <div>
+                    <p>Can't read the image? <a href='javascript: refreshCaptcha();'>click here</a> to refresh</p>
+                </div>
+                <div class="captcha_submit">
+                    <button type="submit" name="submit">Check</button>
+                </div>
+            </form>
+        </div>
+        <script type="text/javascript">
+        function refreshCaptcha() {
+            let img = document.getElementById('captcha_image');
+            let img_attr = img.getAttribute('src');
+            img.src = img_attr.substring(0, img_attr.lastIndexOf('?')) + '?rand=' + Math.random() * 1000;
+        }
+        </script>
+    </div>
+</body>
+
+</html>
